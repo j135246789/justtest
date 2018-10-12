e9242540d41c088ab96491d74296dda29a73395f
j135246789 <j135246789@gmail.com>
2018-10-12 16:07:47 +0800

    test git!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel HI
+                    Laravel getting better!!
                 </div>
 
                 <div class="links">
